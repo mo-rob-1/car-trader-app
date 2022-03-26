ddd517b83cf1020598d8084cb97e5afd63c8f07e
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-03-26 21:17:44 +0000

    connect to database and add routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+
+const port = process.env.PORT || 8000;
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/cars", require("./routes/carRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
