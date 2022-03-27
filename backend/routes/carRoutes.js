192fbe44d004e18994a71b04d5b388b52648c33f
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-03-27 20:22:59 +0100

    protect delete route

--- backend/routes/carRoutes.js
+++ backend/routes/carRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { addCar, getAllCars, getUserCars, getUserCar, updateCar, deleteCar } = require("../controllers/carController");
 const { protect } = require("../middleware/authMiddleware");
 
-router.route("/").post(protect, addCar).get(getAllCars);
-router.route("/:id").delete(deleteCar).put(protect, updateCar).get(protect, getUserCar);
 router.route("/mycars").get(protect, getUserCars);
+router.route("/").post(protect, addCar).get(getAllCars);
+router.route("/:id").delete(protect, deleteCar).put(protect, updateCar).get(protect, getUserCar);
 
 module.exports = router;
