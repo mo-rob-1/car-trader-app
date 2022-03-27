44d593f18a9c4e3f80ec3acaaaaa544f18a14ce4
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-03-27 16:13:10 +0100

    add requests

--- backend/routes/carRoutes.js
+++ backend/routes/carRoutes.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { addCar, getAllCars, getUserCars, getUserCar } = require("../controllers/carController");
+const { addCar, getAllCars, getUserCars, getUserCar, updateCar, deleteCar } = require("../controllers/carController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.route("/").post(protect, addCar).get(getAllCars);
-// router.route("/:id").delete(deleteCar).put(updateCar);
-router.route("/:id").get(protect, getUserCar);
-router.route("/mycars").get(protect, getUserCar);
+router.route("/:id").delete(deleteCar).put(protect, updateCar).get(protect, getUserCar);
+router.route("/mycars").get(protect, getUserCars);
 
 module.exports = router;
