1878b45b70f0c9c60f7e629f21c9473169beaf6f
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-03-28 17:55:49 +0100

    add routes and components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CarListing from "./pages/CarListing";
+import CarDetails from "./pages/CarDetails";
+import Wishlist from "./pages/Wishlist";
+import AddCar from "./pages/AddCar";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div>
-      <h1>Car Trader</h1>
-    </div>
+    <>
+      <Router>
+        <div>
+          <Header />
+          <Routes>
+            <Route path="/" element={<CarListing />} />
+            <Route path="/car/:id" element={<CarDetails />} />
+            <Route path="/addcar" element={<AddCar />} />
+            <Route path="/wishlist" element={<Wishlist />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </>
   );
 }
 
