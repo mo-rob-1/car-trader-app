84ce3a59454d5119695cc65297a4536a96d54d5b
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-03-30 20:31:55 +0100

    add auth and route for logout button

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,21 +1,49 @@
 import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout, reset } from "../features/auth/authSlice";
 
 function Header() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.auth);
+
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate("/");
+  };
+
   return (
     <header>
       <div>
         <Link to="/">Car Trader</Link>
       </div>
       <ul>
-        <li>
-          <button>Logout</button>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
+        {user ? (
+          <>
+            <li>
+              <button className="btn">
+                <Link to="/add-car">Add Car</Link>
+              </button>
+            </li>
+            <li>
+              <button className="btn" onClick={onLogout}>
+                Logout
+              </button>
+            </li>
+          </>
+        ) : (
+          <>
+            {" "}
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </>
+        )}
       </ul>
     </header>
   );
