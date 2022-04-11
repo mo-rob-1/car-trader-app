def9b8c7158a72fa0fd74c9fb6beb417cfcdf4a8
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-04-11 19:13:12 +0100

    update header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -15,25 +15,31 @@ function Header() {
   };
 
   return (
-    <header>
-      <div>
-        <Link to="/">Car Trader</Link>
+    <header className="mx-auto max-w-screen-2xl w-11/12 md:flex md:justify-between md:pt-5 md:pb-5">
+      <div className="text-3xl font-bold md:flex md:justify-center md:items-center">
+        <div className="text-center mt-4 md:mt-0">
+          <Link to="/">Car Trader</Link>
+        </div>
       </div>
-      <ul>
+      <ul className="mt-4 mb-4 flex justify-center items-center">
         {user ? (
           <>
-            <li>
-              <button className="btn">
-                <Link to="/add-car">Add Car</Link>
+            <li className="mr-4 md:mr-10">
+              <button>
+                <Link to="/add-car" className="font-bold">
+                  Add Car
+                </Link>
+              </button>
+            </li>
+            <li className="mr-4 md:mr-10">
+              <button>
+                <Link to="/profile" className="font-bold">
+                  Profile
+                </Link>
               </button>
             </li>
             <li>
-              <button className="btn">
-                <Link to="/mycars">My Cars</Link>
-              </button>
-            </li>
-            <li>
-              <button className="btn" onClick={onLogout}>
+              <button onClick={onLogout} className="font-bold">
                 Logout
               </button>
             </li>
@@ -41,11 +47,15 @@ function Header() {
         ) : (
           <>
             {" "}
-            <li>
-              <Link to="/login">Login</Link>
+            <li className="mr-4 md:mr-10">
+              <Link to="/login" className="font-bold">
+                Login
+              </Link>
             </li>
             <li>
-              <Link to="/register">Register</Link>
+              <Link to="/register" className="font-bold">
+                Register
+              </Link>
             </li>
           </>
         )}
