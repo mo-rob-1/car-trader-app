d398d5bf230f9f27742914a85b4ad638fdd64f77
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-04-12 18:05:33 +0100

    style text

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -50,7 +50,7 @@ function Login() {
   };
 
   if (isLoading) {
-    return <h3>Loading...</h3>;
+    return <h3 className="text-2xl font-bold mt-5 m-auto w-11/12 max-w-screen-2xl">Loading...</h3>;
   }
 
   return (
